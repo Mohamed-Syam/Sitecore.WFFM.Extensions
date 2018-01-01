14ef76f1e593f9b4e3b736d4d13332d41f2d22af
mohammed.syam <mohammed.syam@americaneagle.com>
2018-01-01 14:44:12 +0200

    Add files, initial push

---
+++ Sitecore.WFFM.Extensions/CustomActions/ContactFactory.cs
@@ -0,0 +1,97 @@
+﻿using Sitecore.Analytics;
+using Sitecore.Analytics.Data;
+using Sitecore.Analytics.DataAccess;
+using Sitecore.Analytics.Model;
+using Sitecore.Analytics.Tracking;
+using Sitecore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.WFFM.Extensions.CustomActions
+{
+    public class ContactFactory
+    {
+        private static ContactRepository contactRepository = Sitecore.Configuration.Factory.CreateObject("tracking/contactRepository", true) as ContactRepository;
+        private static ContactManager contactManager = Sitecore.Configuration.Factory.CreateObject("tracking/contactManager", true) as ContactManager;
+
+        public static Contact GetContact(string emailAddress)
+        {
+            if (IsContactInSession(emailAddress))
+            {
+                return Tracker.Current.Session.Contact;
+            }
+
+            var existingContact = contactRepository.LoadContactReadOnly(emailAddress);
+
+            Contact contact;
+
+            if (existingContact != null)
+            {
+                LockAttemptResult<Contact> lockResult = contactManager.TryLoadContact(existingContact.ContactId);
+
+                switch (lockResult.Status)
+                {
+                    case LockAttemptStatus.Success:
+                        Contact lockedContact = lockResult.Object;
+                        lockedContact.ContactSaveMode = ContactSaveMode.AlwaysSave;
+                        contact = lockedContact;
+                        break;
+                    case LockAttemptStatus.NotFound:
+                        contact = Tracker.Current.Session.Contact;
+                        break;
+                    default:
+                        throw new Exception(" Contact could not be locked - " + emailAddress);
+                }
+            }
+            // if Contact in session's identifier is set and not equal to email address create new Contact
+            else if (Tracker.Current.Session.Contact.Identifiers.Identifier != null
+                && !Tracker.Current.Session.Contact.Identifiers.Identifier.Equals(emailAddress, StringComparison.InvariantCultureIgnoreCase))
+            {
+                contact = contactRepository.CreateContact(ID.NewID);
+                contact.System.Value = 0;
+                contact.System.VisitCount = 0;
+                contact.ContactSaveMode = ContactSaveMode.AlwaysSave;
+            }
+            // Contact in session's identifier is null use that Contact
+            else
+            {
+                contact = Tracker.Current.Session.Contact;
+            }
+
+            // If the matched Contact is not as the same as the Contact Sitecore loaded into session 
+            // or the Contact in session is not identified
+            // then we need to call .Identify() - more on this later
+            if (!contact.ContactId.Equals(Tracker.Current.Session.Contact.ContactId)
+                || (contact.ContactId.Equals(Tracker.Current.Session.Contact.ContactId) && Tracker.Current.Session.Contact.Identifiers.Identifier == null))
+            {
+                Tracker.Current.Session.Identify(emailAddress);
+
+                // Contact has been updated via Identify so update the reference
+                contact = Tracker.Current.Session.Contact;
+            }
+
+            return contact;
+        }
+
+        private static bool IsContactInSession(string emailAddress)
+        {
+            var tracker = Tracker.Current;
+
+            if (tracker != null &&
+              tracker.IsActive &&
+              tracker.Session != null &&
+              tracker.Session.Contact != null &&
+              tracker.Session.Contact.Identifiers != null &&
+              tracker.Session.Contact.Identifiers.Identifier != null &&
+              tracker.Session.Contact.Identifiers.Identifier.Equals(emailAddress, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Sitecore.WFFM.Extensions/CustomActions/CustomUpdateContact.cs
@@ -0,0 +1,134 @@
+﻿using Sitecore.Analytics.Tracking;
+using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.WFFM.Abstractions.Actions;
+using Sitecore.WFFM.Abstractions.Analytics;
+using Sitecore.WFFM.Abstractions.Shared;
+using Sitecore.WFFM.Actions.Base;
+using Sitecore.WFFM.Actions.SaveActions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using Sitecore.Analytics;
+using Sitecore.Analytics.Model;
+using Sitecore.Analytics.Data;
+
+namespace Sitecore.WFFM.Extensions.CustomActions
+{
+    public class CustomUpdateContact : WffmSaveAction
+    {
+        private readonly IAnalyticsTracker analyticsTracker;
+        private readonly IAuthentificationManager authentificationManager;
+        private readonly ILogger logger;
+        private readonly IFacetFactory facetFactory;
+
+        public string Mapping { get; set; }
+
+        public CustomUpdateContact() { }
+
+        public CustomUpdateContact(IAnalyticsTracker analyticsTracker, IAuthentificationManager authentificationManager, ILogger logger, IFacetFactory facetFactory)
+        {
+            Assert.IsNotNull((object)analyticsTracker, nameof(analyticsTracker));
+            Assert.IsNotNull((object)authentificationManager, nameof(authentificationManager));
+            Assert.IsNotNull((object)logger, nameof(logger));
+            Assert.IsNotNull((object)facetFactory, nameof(facetFactory));
+            this.analyticsTracker = analyticsTracker;
+            this.authentificationManager = authentificationManager;
+            this.logger = logger;
+            this.facetFactory = facetFactory;
+        }
+
+        [Obsolete]
+        public CustomUpdateContact(IAnalyticsTracker analyticsTracker, IAuthentificationManager authentificationManager, ILogger logger, IFacetFactory facetFactory, IContactManager contactManager)
+      : this(analyticsTracker, authentificationManager, logger, facetFactory)
+        {
+        }
+
+        public override void Execute(ID formId, AdaptedResultList adaptedFields, ActionCallContext actionCallContext = null, params object[] data)
+        {
+            this.UpdateContact(adaptedFields);
+        }
+
+        protected virtual void UpdateContact(AdaptedResultList fields)
+        {
+            Assert.ArgumentNotNull(fields, "adaptedFields");
+            Assert.IsNotNullOrEmpty(this.Mapping, "Empty mapping xml.");
+            Assert.IsNotNull(this.analyticsTracker.CurrentContact, "Tracker.Current.Contact");
+            this.logger.Warn("[UPDATE CONTACT DETAILS Save action] User is not authenticated to edit contact details.", this);
+
+            IEnumerable<FacetNode> enumerable = this.ParseMapping(this.Mapping, fields);
+            IContactFacetFactory contactFacetFactory = this.facetFactory.GetContactFacetFactory();
+
+
+            // Identifying contact if exist
+            var EmailAddressPath = "Emails/Entries/SmtpAddress";
+            string EmailAddressValue = string.Empty;
+            EmailAddressValue = enumerable.Where(node => node.Path == EmailAddressPath).FirstOrDefault().Value;
+            
+            Contact currentContact = Tracker.Current.Session.Contact;
+
+
+            Contact contact = ContactFactory.GetContact(EmailAddressValue);
+            ContactRepository contactRepository = Sitecore.Configuration.Factory.CreateObject("tracking/contactRepository", true) as ContactRepository;
+
+            // The data will be transferred from the dyingContact to the survivingContact
+            contactRepository.MergeContacts(currentContact, contact);
+            
+            IdenitifyContact(EmailAddressValue);
+
+            foreach (FacetNode node in enumerable)
+            {
+                contactFacetFactory.SetFacetValue(currentContact, node.Key, node.Path, node.Value, true);
+            }
+        }
+
+        private void IdenitifyContact(string EmailAddress)
+        {
+            try
+            {
+                if (Sitecore.Analytics.Tracker.Current == null || Sitecore.Analytics.Tracker.Current.Session == null)
+                {
+                    Sitecore.Diagnostics.Log.Info("Trying to identify visitor by email, But Tracker is not initialized ", this);
+                    return;
+                }
+                if (!Sitecore.Analytics.Tracker.IsActive)
+                {
+                    Sitecore.Diagnostics.Log.Info("Trying to identify visitor by email, But Tracker is not Active ", this);
+                    return;
+                }
+
+                if (!string.IsNullOrEmpty(EmailAddress))
+                {
+                    Sitecore.Diagnostics.Log.Info("Identifying vistor by email : '" + EmailAddress.Trim() + "'", this);
+                    Tracker.Current.Session.Identify(EmailAddress.Trim());
+                }
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error(ex.Message, ex, this);
+            }
+        }
+
+        public IEnumerable<FacetNode> ParseMapping(string mapping, AdaptedResultList adaptedFieldResultList)
+        {
+            Assert.ArgumentNotNullOrEmpty(mapping, nameof(mapping));
+            Assert.ArgumentNotNull((object)adaptedFieldResultList, nameof(adaptedFieldResultList));
+            return (IEnumerable<FacetNode>)((object[])new JavaScriptSerializer().Deserialize(mapping, typeof(object))).Cast<Dictionary<string, object>>().Select(item => new
+            {
+                item = item,
+                itemValue = item["value"].ToString()
+            }).Select(_param0 => new
+            {
+                TransparentIdentifier = _param0,
+                itemId = !_param0.item.ContainsKey("id") || _param0.item["id"] == null ? "Preferred" : _param0.item["id"].ToString()
+            }).Select(_param1 => new
+            {
+                TransparentIdentifier1 = _param1,
+                value = adaptedFieldResultList.GetValueByFieldID(ID.Parse(_param1.TransparentIdentifier.item["key"].ToString()))
+            }).Where(_param0 => !string.IsNullOrEmpty(_param0.value)).Select(_param0 => new FacetNode(_param0.TransparentIdentifier1.itemId, _param0.TransparentIdentifier1.TransparentIdentifier.itemValue, _param0.value)).ToList<FacetNode>();
+        }
+    }
+}
